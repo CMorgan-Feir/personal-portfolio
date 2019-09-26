a79550a71397c5256573d2068680080b92d77c6b
CMorgan-Feir <morganfeir@gmail.com>
2019-09-26 17:58:24 -0400

    finalizing 360 project page

--- src/components/articulate-360.js
+++ src/components/articulate-360.js
@@ -4,55 +4,31 @@ class Articulate360 extends Component {
     render() {
         return (
             <div className="project-page-container">
-            <div>
-                {/* <div className="the-div-that-holds-the-divs">
-                    <div className="project-row-one">
-                        <div className="holder-div" width="300px">
-                            <video loop autoPlay>
-                                <source src="https://morganfeir.s3.us-east-2.amazonaws.com/portfolio/articulate-360.mp4" tyle="video/mp4" />
-                            </video>
-                            <img className="project-image" src="https://morganfeir.s3.us-east-2.amazonaws.com/portfolio/articulate-360-2.png"></img>
-                        </div>
-                        <div className="holder-div">
-                            <img className="project-image" src="https://morganfeir.s3.us-east-2.amazonaws.com/portfolio/articulate-360-1.png"></img>
-                        </div>
-                    </div>
-                    <div className="project-row-two">
-                        <div className="holder-div">
-                            <img className="project-image" src="https://morganfeir.s3.us-east-2.amazonaws.com/portfolio/articulate-360-3.png"></img>
-                        </div>
-                        <div className="holder-div">
-                            <img className="project-image" src="https://morganfeir.s3.us-east-2.amazonaws.com/portfolio/articulate-360-4.png"></img>
-                        </div>
-                    </div>
-                </div> */}
                 <div className="video-container">
                     <video className="video-content" loop autoPlay>
                         <source src="https://morganfeir.s3.us-east-2.amazonaws.com/portfolio/articulate-360.mp4" tyle="video/mp4" />
                     </video>
-                    {/* <img className="project-image" src="https://morganfeir.s3.us-east-2.amazonaws.com/portfolio/articulate-360-2.png"></img> */}
                 </div>
-            </div>
-            <div className="project-details-container">
-                <div className="project-details-header">
-                    <h1>An otherworldly art experience</h1>
+                <div className="project-details-container">
+                    <div className="project-details-header">
+                        <h1>An otherworldly art experience</h1>
+                    </div>
+                    <div className="project-details-more-info" />
                 </div>
-                <div className="project-details-more-info" />
-            </div>
-            <div className="tech-stack-details">
-                <span className="the-brief-header-text"><h3>The Brief</h3></span>
-                <p>
-                Works of art are fragile, hard to ship, expensive and rare. Few people can afford to travel to all the art they would like to see, and digital images rarely do justice to the real thing. Articulate 360 is meant to bridge this gap: an immersive VR experience where works of art come to like with careful animations in the digital realm.
-                </p>
-                <span className="the-role-header-text"><h3>The Role</h3></span>
-                <p>
-                Sole developer
-                </p>
-                <span className="the-tech-header-text"><h3>The Tech</h3></span>
-                <p>
-                JavaScript, HTML, React 360, Git and plenty of math
-                </p>
-            </div>
+                <div className="tech-stack-details">
+                    <span className="the-brief-header-text"><h3>The Brief</h3></span>
+                    <p>
+                        Works of art are fragile, hard to ship, expensive and rare. Few people can afford to travel to all the art they would like to see, and digital images rarely do justice to the real thing. Articulate 360 is meant to bridge this gap: an immersive VR experience where works of art come to like with careful animations in the digital realm.
+                    </p>
+                    <span className="the-role-header-text"><h3>The Role</h3></span>
+                    <p>
+                        Sole developer
+                    </p>
+                    <span className="the-tech-header-text"><h3>The Tech</h3></span>
+                    <p>
+                        JavaScript, HTML, React 360, Git and plenty of math
+                    </p>
+                </div>
             </div>
         )
     }
